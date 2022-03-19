b917cb0539c62ee531f6701cffa06def2f1ad0c6
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2022-03-18 18:09:59 -0600

    updated repo to new remote origin

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -1,11 +1,11 @@
 import React from "react";
-import resume from "../../assets/resume/Chris_Bradford_Resume.PDF";
+import resume from "../../assets/resume/Resume_Chris_Bradford.PDF";
 function Resume() {
   return (
     <section className="resume">
       <br />
       <div>
-        <a className="download" href={resume} download="Chris_Bradford_Resume.PDF">
+        <a className="download" href={resume} download="Resume_Chris_Bradford.PDF">
           Download My Resume
         </a>
       </div>
