599b146f4c665a5430b97c0dd416379e51aa61ba
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2022-03-29 17:37:10 -0600

    removed some stuff from resume component

--- src/components/Resume/resume.js
+++ src/components/Resume/resume.js
@@ -9,24 +9,6 @@ function Resume() {
           Download My Resume
         </a>
       </div>
-      <div className="profifs">
-        <h2>Proficiencies</h2>
-        <ul className="skills">
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
-      </div>
     </section>
   );
 }
